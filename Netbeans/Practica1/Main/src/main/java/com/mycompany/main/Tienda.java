a647013b27198d6f141affa403de788c1cd0bbaf
PolyNomius09 <rlopezo1900@alumno.ipn.mx>
2023-02-27 17:14:17 -0600

    Añadi referencias y el repo de las referencias

--- Netbeans/Practica1/Main/src/main/java/com/mycompany/main/Tienda.java
+++ Netbeans/Practica1/Main/src/main/java/com/mycompany/main/Tienda.java
@@ -1,6 +1,17 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+
+* Coloco referencias de como funciona la libreria Scanner porque tiene desde 3er semestre de preparatoria que no programo
+* en Java, y no recordaba para que funcionaban unas cosas, perdon :'c
+
+//referencias: sc.nextLine http://puntocomnoesunlenguaje.blogspot.com/2012/08/java-scanner.html
+//referencias: https://www.delftstack.com/es/howto/java/java-next-and-nextline/
+//referencias: https://www.geeksforgeeks.org/string-tostring-method-in-java/
+//referencias: https://ifgeekthen.nttdata.com/es/que-es-y-como-usar-la-clase-scanner-en-java
+//referencias: Use como referencias, codigos de c/c++ de clases pasadas, se deja el repositorio.
+//Repositorio: https://github.com/PolyNomius09/ProyectosDeClase.git
+
  */
 package com.mycompany.main;
 
@@ -160,6 +171,3 @@ class Tienda {
     }
   }
 }
-
-//referencias: sc.nextLine http://puntocomnoesunlenguaje.blogspot.com/2012/08/java-scanner.html
-//referencias: https://www.delftstack.com/es/howto/java/java-next-and-nextline/
