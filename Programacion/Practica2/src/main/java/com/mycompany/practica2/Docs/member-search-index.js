02bfbc93166abea5141a14c0bda78d4f11ae6650
PolyNomius09 <rlopezo1900@alumno.ipn.mx>
2023-03-01 10:57:29 -0600

    Arreglando Fallos y añadimos documentacion del codigo

---
+++ Programacion/Practica2/src/main/java/com/mycompany/practica2/Docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.mycompany.practica2","c":"Practica2","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"com.mycompany.practica2","c":"Practica2","l":"Practica2()","u":"%3Cinit%3E()"}];updateSearchResults();
